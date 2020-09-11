16529f9cd969100161a773268495de7c800fd9a4
Omer Uddin <kickash32@gmail.com>
2020-09-10 21:48:22 -0400

    typo

--- Demo.js
+++ Demo.js
@@ -2,7 +2,7 @@ import { GoBack } from "./GoBack.js";
 
 function addGoBack() {
     document.querySelectorAll("#goBack").forEach(function(element) {
-        element.onclick = GoBack.GoBack;
+        element.onclick = GoBack.goBack;
     });
 }
 
